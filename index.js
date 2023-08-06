c454d98f6e3c496e127d2caa78de540f97675d38
vimal199 <vimalbhardwaj8@gmail.com>
2023-08-06 12:24:18 +0530

    Adding Validation for schema

--- index.js
+++ index.js
@@ -102,16 +102,16 @@ app.delete('/api/notes/:id', (request, response, next) => {
         .catch(error => next(error))
 }
 )
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
 
     const body = request.body
-    if (!body.content) {
+    /*if (!body.content) {
         return response.status(400).json(
             {
                 error: 'content missing'
             }
         )
-    }
+    }*/
     const note = new Note({
         content: body.content,
         important: body.important || false,
@@ -124,7 +124,7 @@ app.post('/api/notes', (request, response) => {
     note.save().then(savedNode => {
         response.json(note)
     }
-    )
+    ).catch(error => next(error))
 }
 
 )
@@ -140,7 +140,7 @@ app.put('/api/notes/:id', (request, response, next) => {
         content: body.content,
         important: body.important,
     }
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true, context: 'query' })
         .then(updatedNote => {
             response.json(updatedNote)
         })
@@ -151,6 +151,8 @@ const errorHandler = (error, request, response, next) => {
     console.log(error.message)
     if (error.name === 'CastError') {
         return response.status(404).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
     next(error)
 }
